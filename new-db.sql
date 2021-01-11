fa30d47d8af7bdf982531e272b1f38074755d4d5
phaneendra-github <phanilucky999@gmail.com>
2021-01-11 15:18:01 +0530

    new database

---
+++ new-db.sql
@@ -0,0 +1,14 @@
+create database campemployee;
+use campemployee;
+CREATE TABLE hyderabad
+( employee_id INT NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  first_name VARCHAR(30),
+  salary VARCHAR(30),
+  phone INT NOT NULL,
+  department VARCHAR(30),
+  emp_role VARCHAR(30)
+);
+
+INSERT into hyderabad values ('1', 'kumar' ,'arun', '1000000', '999', 'devops', 'architect' );
+
